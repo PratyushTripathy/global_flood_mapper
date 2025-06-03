d71d8a623693047d7d5327cb694f84d44787a758
Pratyush Tripathy <pratkrt@gmail.com>
2025-06-02 21:07:22 -0700

    added license

---
+++ LICENSE.m
@@ -0,0 +1,9 @@
+Copyright (c) 2024 Pratyush Tripathy
+
+Permission is granted to use, copy, modify, and distribute this software, code, and associated materials for academic research and other non-commercial purposes only, provided that this copyright notice and this permission notice appear in all copies.
+
+Commercial use of this software, code, web tool, or any derivatives is strictly prohibited without explicit, written permission from the copyright holder.
+
+This software and associated materials are provided "as is", without warranty of any kind, express or implied. In no event shall the authors or copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software, code, or materials or the use or other dealings in them.
+
+For inquiries about commercial licensing, please contact: pratkrt@gmail.com
